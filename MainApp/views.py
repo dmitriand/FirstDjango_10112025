6595b652bc1ea215fba90b2ff20ea563bd4f51ff
Andrey Dmitriev <dmitriand@gmail.com>
2025-11-13 14:14:59 +0300

    M3:orm1_del_global_items

--- MainApp/views.py
+++ MainApp/views.py
@@ -5,15 +5,6 @@ from .models import Item
 # Create your views here.
 
 
-# items = [
-#     {"id": 1, "name": "Кроссовки abibas", "quantity": 5},
-#     {"id": 2, "name": "Куртка кожаная", "quantity": 2},
-#     {"id": 5, "name": "Coca-cola 1 литр", "quantity": 12},
-#     {"id": 7, "name": "Картофель фри", "quantity": 0},
-#     {"id": 8, "name": "Кепка", "quantity": 124},
-# ]
-
-
 def home(request):
     context = {"name": "Иванов Иван Николаевич", "email": "my_mail@mail.ru"}
     return render(request, "index.html", context)
