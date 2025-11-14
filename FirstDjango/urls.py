30ea6c648c84598b1ebdaf0480ecf8f6537d37fa
Andrey Dmitriev <dmitriand@gmail.com>
2025-11-14 10:08:45 +0300

    Done. M4: part1.3

--- FirstDjango/urls.py
+++ FirstDjango/urls.py
@@ -4,7 +4,7 @@ from MainApp import views
 
 urlpatterns = [
     path("", views.home),
-    path("about/", views.about),
-    path("item/<int:id>/", views.get_item),
-    path("items/", views.get_all_items),
+    path("about/", views.about, name="about"),
+    path("item/<int:id>/", views.get_item, name="item-detail"),
+    path("items/", views.get_all_items, name="all_items"),
 ]
